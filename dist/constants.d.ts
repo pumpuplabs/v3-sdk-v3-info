f7eeb2570fa62876e85975188cbf887f2127d649
kkkkk-npc <kkkkknpc@proton.me>
2024-09-08 22:18:12 +0800

    feat:update build

---
+++ dist/constants.d.ts
@@ -0,0 +1,27 @@
+import { ChainId } from '@uniswap/sdk-core';
+export declare const FACTORY_ADDRESS = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
+export declare const FACTORY_ADDRESS_MAP: {
+    84532: string;
+    84531: string;
+    43114: string;
+    137: string;
+    1: string;
+    8453: string;
+};
+export declare const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export declare const POOL_INIT_CODE_HASH = "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54";
+/**
+ * The default factory enabled fee amounts, denominated in hundredths of bips.
+ */
+export declare enum FeeAmount {
+    LOWEST = 100,
+    LOW = 500,
+    MEDIUM = 3000,
+    HIGH = 10000
+}
+/**
+ * The default factory tick spacings by fee amount.
+ */
+export declare const TICK_SPACINGS: {
+    [amount in FeeAmount]: number;
+};
