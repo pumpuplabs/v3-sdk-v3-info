aa15003115cea1d38216e744a428f3559b0f7f22
kkkkk-npc <kkkkknpc@proton.me>
2024-09-14 19:48:25 +0800

    bump v3-periphery to 1.0.4 and v3-core to 1.0.1

--- src/constants.ts
+++ src/constants.ts
@@ -2,17 +2,17 @@ import { ChainId } from '@uniswap/sdk-core'
 
 export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
 export const FACTORY_ADDRESS_MAP = {
-  [ChainId.BASE_SEPOLIA]: '0xD38A464A4446ea42A6d7EdC68A24e175b4Ff38Ae',
+  [ChainId.BASE_SEPOLIA]: '0x235a0f30B8D3338E5046b2e9cDb62E5ce498d3A1',
   [ChainId.BASE_GOERLI]: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
   [ChainId.AVALANCHE]: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
   [ChainId.POLYGON]: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
   [ChainId.MAINNET]: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
-  [ChainId.BASE]: '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+  [ChainId.BASE]: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
 }
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0x1e85f501b45deb56047d2d981e42784125caf47d6c30afeaab8a2a9fc0498037'
+export const POOL_INIT_CODE_HASH = '0x4f157d8bc516d785f45049f8f9027de387954a1002431d9eac3d87b5f08a1976'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
@@ -21,7 +21,8 @@ export enum FeeAmount {
   LOWEST = 100,
   LOW = 500,
   MEDIUM = 3000,
-  HIGH = 10000
+  HIGH = 10000,
+  HIGHEST = 18000,
 }
 
 /**
@@ -31,5 +32,6 @@ export const TICK_SPACINGS: { [amount in FeeAmount]: number } = {
   [FeeAmount.LOWEST]: 1,
   [FeeAmount.LOW]: 10,
   [FeeAmount.MEDIUM]: 60,
-  [FeeAmount.HIGH]: 200
+  [FeeAmount.HIGH]: 200,
+  [FeeAmount.HIGHEST]: 360,
 }
